ed0c69e12678ac681896f194f52dcac1ad663e50
Zambrano <jesus.zambrano@cetys.edu.mx>
2023-05-19 20:41:50 -0700

    training codigo faltante

--- proyecto_2_DL/training.py
+++ proyecto_2_DL/training.py
@@ -32,10 +32,12 @@ def validation_step(val_loader, net, cost_function):
         batch_labels = batch_labels.to(device)
         with torch.inference_mode():
             # TODO: realiza un forward pass, calcula el loss y acumula el costo
-
+            batch_preds = net(batch_imgs.to(device))
+            loss = cost_function(batch_preds, batch_labels)
+            val_loss += loss.item()
             
     # TODO: Regresa el costo promedio por minibatch
-    return
+    return val_loss / len(val_loader)
 
 def train():
     # Hyperparametros
@@ -60,7 +62,7 @@ def train():
                      n_classes = 7)
 
     # TODO: Define la funcion de costo
-    criterion = 
+    criterion = nn.CrossEntropyLoss()
 
     # Define el optimizador
     optimizer = optim.Adam(modelo.parameters(), lr=learning_rate)
@@ -73,17 +75,23 @@ def train():
             batch_labels = batch['label']
             # TODO Zero grad, forward pass, backward pass, optimizer step
             optimizer.zero_grad()
-
+            batch_preds = modelo(batch_imgs)
+            loss = criterion(batch_preds, batch_labels)
+            loss.backward()
+            optimizer.step()
 
             # TODO acumula el costo
-
+            train_loss += loss.item()
 
         # TODO Calcula el costo promedio
-        train_loss = 
+        train_loss /= len(train_loader)
         val_loss = validation_step(val_loader, modelo, criterion)
         tqdm.write(f"Epoch: {epoch}, train_loss: {train_loss:.2f}, val_loss: {val_loss:.2f}")
 
         # TODO guarda el modelo si el costo de validación es menor al mejor costo de validación
+        if val_loss < best_epoch_loss:
+            best_epoch_loss = val_loss
+            torch.save(modelo.state_dict(), "best_model.pth")
         
         plotter.on_epoch_end(epoch, train_loss, val_loss)
     plotter.on_train_end()
