3fa238b4f91343b35a199adde72b59cc742ccf47
Zambrano <jesus.zambrano@cetys.edu.mx>
2023-05-19 21:43:34 -0700

    no se si esta bien pero es algo

--- proyecto_2_DL/network.py
+++ proyecto_2_DL/network.py
@@ -37,6 +37,27 @@ class Network(nn.Module):
 
     def forward(self, x: torch.Tensor) -> torch.Tensor:
         # TODO: Define la propagacion hacia adelante de tu red
+        x = F.relu(self.conv1(x))
+        x = self.pool(x)
+        x = F.relu(self.conv2(x))
+        x = self.pool(x)
+        x = F.relu(self.conv3(x))
+        x = self.pool(x)
+        x = F.relu(self.conv4(x))
+        x = self.pool(x)
+        x = F.relu(self.conv5(x))
+        x = self.pool(x)
+
+        x = x.view(x.size(0), -1)
+
+        x = F.relu(self.fc1(x))
+        x = self.dropout(x)
+        x = F.relu(self.fc2(x))
+        x = self.dropout(x)
+
+        logits = self.fc3(x)
+        proba = F.softmax(logits, dim=1)
+
         return logits, proba 
 
     def predict(self, x):
